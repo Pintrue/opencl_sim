09e759f6d7ea5c86844b632abdc3e5895a19bdf3
Pintrue <y451059931@icloud.com>
2019-08-22 21:25:53 +0100

    move input, output data structures to headers

--- sim/host/src/host.hpp
+++ sim/host/src/host.hpp
@@ -3,6 +3,28 @@
 
 #include "CL/opencl.h"
 
+
+// Runtime config
+cl_platform_id platform = NULL;
+cl_device_id device;
+cl_context context = NULL;
+cl_command_queue command_queue;
+cl_program program = NULL;
+cl_kernel kernel;
+cl_kernel km_kernel;
+
+cl_mem input_jnt_angles_buf;
+cl_mem output_trig_vals_buf;
+cl_mem output_ee_pose_buf;
+
+// Input data
+uint* input_jnt_angles;
+ulong* output_trig_vals;
+
+// output data
+ulong* output_ee_pose;
+
+
 void checkStatus(cl_int status, const char* file, int line, const char* msg);
 double randAngleRads(double lower, double upper);
 uint convertRadsToInt(double radians);
@@ -12,7 +34,7 @@ void initInput();
 void initInput(double jnt_angles[3]);
 void initKMInput();
 void run();
-void runKM(double ee_pos[3]);
+void runKM();
 void cleanup();
 
 #endif
